d1b1e069368a2e3779d07eebcbea83e5cd61e22d
bishal <234aparnapaul@gmail.com>
2024-12-26 11:05:12 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero";
+import About from "./components/About"
+import Navbar from "./components/Navbar"
+import Features from "./components/Features"
+import FloatingImage from "./components/Story"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+export default function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+       <Navbar/>
+       <Hero />
+       <About/>
+       <Features/>
+       <FloatingImage/>
+       <Contact/>
+       <Footer/>
+    </main>
+  )
+}
